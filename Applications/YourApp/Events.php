590d1cb47f5381fa84c02d4f1ed4b7d7be66edd0
phpfull <285303886@qq.com>
2018-03-08 16:38:00 +0800

    调试websocket

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -28,33 +28,42 @@ use \GatewayWorker\Lib\Gateway;
  */
 class Events
 {
+
     /**
      * 当客户端连接时触发
      * 如果业务不需此回调可以删除onConnect
-     * 
-     * @param int $client_id 连接id
+     * @param $client_id string 连接id
+     * @throws Exception
      */
     public static function onConnect($client_id) {
-        // 向当前client_id发送数据 
+/*        // 向当前client_id发送数据
         Gateway::sendToClient($client_id, "Hello $client_id\n");
         // 向所有人发送
-        Gateway::sendToAll("$client_id login\n");
+        Gateway::sendToAll("$client_id login\n");*/
+        Gateway::sendToClient($client_id, json_encode(array(
+            'type'      => 'init',
+            'clientId' => $client_id
+        )));
     }
     
-   /**
-    * 当客户端发来消息时触发
-    * @param int $client_id 连接id
-    * @param mixed $message 具体消息
-    */
+
+    /**
+     * 当客户端发来消息时触发
+     * @param $client_id string 连接id
+     * @param $message  string 具体消息
+     * @throws Exception
+     */
    public static function onMessage($client_id, $message) {
         // 向所有人发送 
-        Gateway::sendToAll("$client_id said $message");
+        //Gateway::sendToAll("$client_id said $message");
    }
    
-   /**
-    * 当用户断开连接时触发
-    * @param int $client_id 连接id
-    */
+
+    /**
+     * 当用户断开连接时触发
+     * @param $client_id string 连接id
+     * @throws Exception
+     */
    public static function onClose($client_id) {
        // 向所有人发送 
        GateWay::sendToAll("$client_id logout");
