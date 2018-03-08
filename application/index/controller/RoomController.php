667022aaf39bafef992dd64f77b0703a2e8f404a
phpfull <285303886@qq.com>
2018-03-08 16:27:02 +0800

    增加连接和聊天

--- application/index/controller/RoomController.php
+++ application/index/controller/RoomController.php
@@ -2,9 +2,37 @@
 
 namespace app\index\controller;
 
+use GatewayClient\Gateway;
+
 class RoomController extends CommonController {
     public function index() {
+        $roomId = input('param.roomId', 1, 'intval'); //默认房间号为1
+        $this->assign('roomId', $roomId);
         $this->assign('user', $this->user);
         return view();
+    }
+
+    public function bind() {
+        $user = $this->user;
+        $userId = $user['id'];
+        $clientId = input('post.clientId', '');
+        $roomId = input('post.roomId', 1, 'intval'); //默认房间号为1
+        Gateway::$registerAddress='127.0.0.1:1238';
+        Gateway::bindUid($clientId, $userId);
+        Gateway::joinGroup($clientId,$roomId);
+        $this->responseSuccess(['userId'=>$userId]);
+    }
+
+    public function talk() {
+        Gateway::$registerAddress='127.0.0.1:1238';
+        $user = $this->user;
+        $userId = $user['id'];
+        $roomId = input('post.roomId', 1, 'intval'); //默认房间号为1
+        $message = input('post.message', '', 'trim');
+        $data = json_encode([
+            'message'=>$message,
+            'userId'=>$userId
+        ]);
+        Gateway::sendToGroup($roomId, $data);
     }
 }
