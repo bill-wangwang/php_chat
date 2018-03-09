a277879813f4bd87c7c006b6e556710e4ec828b3
phpfull <285303886@qq.com>
2018-03-09 15:20:47 +0800

    增加心跳检测

--- application/index/controller/RoomController.php
+++ application/index/controller/RoomController.php
@@ -39,4 +39,27 @@ class RoomController extends CommonController {
         Gateway::sendToGroup($roomId, $data);
         $this->responseSuccess();
     }
+
+    public function debug() {
+        Gateway::$registerAddress='127.0.0.1:1238';
+        $user = $this->user;
+        $userId = $user['id'];
+        $message = "这是一条群发消息";
+        $data = json_encode([
+            'message'=>$message,
+            'userId'=>$userId,
+            'avatar'=> '/static/avatar/' . (($userId % 70)+1) . '.jpg',
+            'account'=>$user['account'],
+            'date'=>date('H:i:s')
+        ]);
+        Gateway::sendToAll($data);
+        for($i=1;$i<9;$i++){
+            echo "user={$i}在线状态：" . Gateway::isUidOnline($i) . '<Br/>';
+            echo "user={$i}的clientId为：" . Gateway::getClientIdByUid($i) . '<Br/>';
+        }
+        echo "当前用户数(clientId)：" . Gateway::getAllClientCount() . '<BR/>';
+        print_r(Gateway::getAllClientSessions());
+        print_r(Gateway::getAllClientInfo());
+
+    }
 }
